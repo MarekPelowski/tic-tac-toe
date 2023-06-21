ed8906b4736dc62fa815ed1f0e2f36e2dec7ede2
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-21 15:42:50 +0200

    fixed a typo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root.geometry("500x600")
 root.resizable(0, 0)
 root.configure(bg="#575757")
 
-overlay_image = PhotoImage(file = r"overlay.png")
+overlay_image = PhotoImage(file=r"overlay.png")
 
 overlay = Label(root, image=overlay_image, bg="#575757", fg="#575757", borderwidth=0)
 overlay.pack()
