183de4c5e83217cd8b9b06c068d6706f5f853d85
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-21 15:41:26 +0200

    added the overlay to the programm

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ root.geometry("500x550")
 root.resizable(0, 0)
 root.configure(bg="#575757")
 
+overlay_image = PhotoImage(file = r"overlay.png")
+
+overlay = Label(root, image=overlay_image, bg="#575757", fg="#575757", borderwidth=0)
+overlay.pack()
+
 root.mainloop()
