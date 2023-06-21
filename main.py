64e3b9380a2af6afcad637b42a144d9c3505e6f8
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-21 15:58:12 +0200

    added circle and cross to the program

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ root.resizable(0, 0)
 root.configure(bg="#575757")
 
 overlay_image = PhotoImage(file=r"overlay.png")
+cross_image = PhotoImage(file=r"cross.png")
+circle_image = PhotoImage(file=r"circle.png")
+
 
 overlay = Label(root, image=overlay_image, bg="#575757", fg="#575757", borderwidth=0)
 overlay.pack()
