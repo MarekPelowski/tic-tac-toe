4b90ca79dad6e47fffc1ef458a9398f7a7ce2623
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-24 18:28:30 +0200

    made the "X"

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ root.configure(bg="#575757")
 
 def button_click(num):
     if num == 1:
-        pass
+        square_1.configure(image=cross_image, height=165, width=160, state=DISABLED)
     elif num == 2:
-        pass
+        square_2.configure(image=cross_image, height=165, width=150, state=DISABLED)
     elif num == 3:
-        pass
+        square_3.configure(image=cross_image, height=165, width=160, state=DISABLED)
     elif num == 4:
-        pass
+        square_4.configure(image=cross_image, height=140, width=160, state=DISABLED)
     elif num == 5:
-        pass
+        square_5.configure(image=cross_image, height=140, width=150, state=DISABLED)
     elif num == 6:
-        pass
+        square_6.configure(image=cross_image, height=140, width=160, state=DISABLED)
     elif num == 7:
-        pass
+        square_7.configure(image=cross_image, height=155, width=160, state=DISABLED)
     elif num == 8:
-        pass
+        square_8.configure(image=cross_image, height=155, width=150, state=DISABLED)
     elif num == 9:
-        pass
+        square_9.configure(image=cross_image, height=155, width=160, state=DISABLED)
+
 
 overlay_image = PhotoImage(file=r"overlay.png")
 cross_image = PhotoImage(file=r"cross.png")
@@ -32,31 +33,31 @@ circle_image = PhotoImage(file=r"circle.png")
 overlay = Label(root, image=overlay_image, bg="#575757", fg="#575757", borderwidth=0)
 overlay.pack()
 
-square_1 = Button(root, height=10, width=22, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(1))
+square_1 = Button(root, height=10, width=22, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(1))
 square_1.place(x=0, y=0)
 
-square_2 = Button(root, height=10, width=20, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(2))
+square_2 = Button(root, height=10, width=20, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(2))
 square_2.place(x=173, y=0)
 
-square_3 = Button(root, height=10, width=24, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(3))
+square_3 = Button(root, height=10, width=24, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(3))
 square_3.place(x=334, y=0)
 
-square_4 = Button(root, height=9, width=22, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(4))
+square_4 = Button(root, height=9, width=22, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(4))
 square_4.place(x=0, y=175)
 
-square_5 = Button(root, height=9, width=20, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(5))
+square_5 = Button(root, height=9, width=20, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(5))
 square_5.place(x=173, y=175)
 
-square_6 = Button(root, height=9, width=24, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(6))
+square_6 = Button(root, height=9, width=24, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(6))
 square_6.place(x=334, y=175)
 
-square_7 = Button(root, height=10, width=22, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(7))
+square_7 = Button(root, height=10, width=22, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(7))
 square_7.place(x=0, y=335)
 
-square_8 = Button(root, height=10, width=20, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(8))
+square_8 = Button(root, height=10, width=20, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(8))
 square_8.place(x=173, y=335)
 
-square_9 = Button(root, height=10, width=24, bg="green", fg="#575757", borderwidth=0, command=lambda: button_click(9))
+square_9 = Button(root, height=10, width=24, bg="#575757", fg="#575757", borderwidth=0, command=lambda: button_click(9))
 square_9.place(x=334, y=335)
 
 
